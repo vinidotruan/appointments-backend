c45fc5fe6538cdd0ee775f585a2b25f60d55f10e
Ruan Vinícius <contatoruanvinicius@gmail.com>
2020-07-27 00:00:33 -0300

    - Add migration to add 'receipt' field in users table

--- database/migrations/2020_07_27_022046_add_receipt_to_users_table.php
+++ database/migrations/2020_07_27_022046_add_receipt_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddStringsToUsersTable extends Migration
+class AddReceiptToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddStringsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->boolean('receipt')->default(false);
         });
     }
 
