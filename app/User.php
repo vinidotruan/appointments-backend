84121e61050808de25fce3fa546770fd7a732955
Ruan Vinícius <contatoruanvinicius@gmail.com>
2020-07-27 00:01:17 -0300

    - Add 'receipt' to fillable fields'

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'role_id', 'gender_id', 'value'
+        'name', 'email', 'password', 'role_id', 'gender_id', 'value', 'receipt'
     ];
 
     /**
